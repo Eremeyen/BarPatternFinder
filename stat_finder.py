cd657d5d2291c46feff555711a151f319b1e8cac
eremeyen <erenonder1905@gmail.com>
2022-08-05 11:44:44 +0300

    generated .json files for pattern results

--- stat_finder.py
+++ stat_finder.py
@@ -4,18 +4,20 @@
 import green_red_pattern_finder
 import statistics
 import os
+import json
 #import mathplotlib
 
 
 
 #example/rough code
+"""
 filename = "BTCUSDT_4h_2022H1.CSV"
 
 candles = green_red_pattern_finder.candles_from_CSV(filename)
 length_three_p = green_red_pattern_finder.lengthThreeKlines(candles)
 length_four_p = green_red_pattern_finder.lengthFourKlines(candles)
 length_five_p = green_red_pattern_finder.lengthFiveKlines(candles)
-#print(length_five_p)
+"""
 
 #receives the dictionaries as inputs and outputs another dictionary
 def statsDictionary(dic):
@@ -24,29 +26,71 @@ def statsDictionary(dic):
         dict[key] = {}
         r_arr = []
         for i in dic[key]:
-            print(i)
-            r_arr.append(i.following_candle.ratio)
-        print(len(r_arr))
-        dict[key]["average return"] = sum(r_arr)/len(r_arr)
-        dict[key]["median return"] = statistics.median(r_arr)
+            r_arr.append(i.following_candle.ratio*100)
+        if(len(r_arr) != 0):
+            dict[key]["average return"] = sum(r_arr)/len(r_arr)
+        else:
+            dict[key]["average return"] = "this pattern wasn't observed"
+        if(len(r_arr) != 0):
+            dict[key]["median return"] = statistics.median(r_arr)
+        else:
+            dict[key]["median return"] = "this pattern wasn't observed"
+        #dict[key]["median return"] = statistics.median(r_arr)
         dict[key]["number"] = len(dic[key])
         count = 0
         for i in dic[key]:
             if(i.following_candle.is_green):
                 count +=1
-        dict[key]["percentage of green candles"] = count / len(dic[key]) * 100
+        #dict[key]["percentage of green candles"] = 
+        dict[key]["percengage of green candles"] = count / len(dic[key]) * 100 if len(r_arr) > 0 else "this pattern wasn't observed"
     return dict
 
 
 
-five = statsDictionary(length_five_p)
-print(five)
+#five = statsDictionary(length_five_p)
 
+#sample saving as json file:
+
+"""
+json = json.dumps(five)
+jsonwriter = open("Pattern_Results/test.json","w")
+jsonwriter.write(json)
+jsonwriter.close
+"""
+
+def generate_json():
+    for file in os.listdir("DATA"):
+        candles = green_red_pattern_finder.candles_from_CSV(file)
+        three_klines = green_red_pattern_finder.lengthThreeKlines(candles)
+        three = statsDictionary(three_klines)
+
+        four_klines = green_red_pattern_finder.lengthFourKlines(candles)
+        four = statsDictionary(four_klines)
+
+        five_klines = green_red_pattern_finder.lengthFiveKlines(candles)
+        five = statsDictionary(five_klines)
+        
+        j = json.dumps(three)
+        jsonwriter = open("Pattern_Results/" + "three" + file[:-4] +".json","w")
+        jsonwriter.write(j)
+        jsonwriter.close
+
+        j = json.dumps(four)
+        jsonwriter = open("Pattern_Results/" + "four" + file[:-4] +".json","w")
+        jsonwriter.write(j)
+        jsonwriter.close
+
+        j = json.dumps(five)
+        jsonwriter = open("Pattern_Results/" + "five" + file[:-4] +".json","w")
+        jsonwriter.write(j)
+        jsonwriter.close
+
+generate_json()
 #idea: sort list of returns?
 #store timestamps as well?
 #use numpy arrays instead?
 
-    
+
 
 
 
