128ecfc26ed6f1268d74cbbeea83bc3d19357f05
eremeyen <erenonder1905@gmail.com>
2022-08-06 00:30:30 +0300

    added test pyplot.scatter functionality

--- plotter.py
+++ plotter.py
@@ -1,4 +1,4 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import json
 import os
 import green_red_pattern_finder
@@ -14,7 +14,13 @@ for key in l_three:
     returns_dict[key] = []
     for kline in l_three[key]:
         returns_dict[key].append(kline.following_candle.ratio)
+count = 0
+x_axis = []
+for i in returns_dict["[True, True, False]"]:
+    x_axis.append(count)
+    count += 1
 
-plt.plot(returns_dict[True,True,False])
-plt.ylabel('return values')
-plt.show
+
+plt.scatter(x_axis,returns_dict["[True, True, False]"], c="blue")
+plt.savefig("plots/test.png")
+#plt.show()
