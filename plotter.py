efe182a9134b795241356e5a2a9f7bf8be6abc43
Eremeyen <60618914+Eremeyen@users.noreply.github.com>
2022-08-07 00:30:06 +0300

    added functionality for four and five kline length plotting

--- plotter.py
+++ plotter.py
@@ -9,9 +9,44 @@ filename = ("BTCUSDT_1d_2017.csv")
 def getPlots(filename):
     candles = green_red_pattern_finder.candles_from_CSV(filename)
     l_three = green_red_pattern_finder.lengthThreeKlines(candles)
+    l_four = green_red_pattern_finder.lengthFourKlines(candles)
+    l_five = green_red_pattern_finder.lengthFiveKlines(candles)
 
     returns_dict = {}
     for key in l_three:
+        returns_dict[key] = []
+        for kline in l_three[key]:
+            returns_dict[key].append(kline.following_candle.ratio)
+    for key in returns_dict:
+        x_axis = []
+        count = 0
+        for i in returns_dict[key]:
+            x_axis.append(count)
+            count +=1
+        plt.ylabel("following candle return")
+        plt.xlabel("chronological order")
+        plt.title(key + filename[:-4])
+        plt.scatter(x_axis,returns_dict[key],c="blue")
+        plt.savefig("plots/" + key + filename[:-4] + ".png")
+    
+    returns_dict = {}
+    for key in l_four:
+        returns_dict[key] = []
+        for kline in l_three[key]:
+            returns_dict[key].append(kline.following_candle.ratio)
+    for key in returns_dict:
+        x_axis = []
+        count = 0
+        for i in returns_dict[key]:
+            x_axis.append(count)
+            count +=1
+        plt.ylabel("following candle return")
+        plt.xlabel("chronological order")
+        plt.title(key + filename[:-4])
+        plt.scatter(x_axis,returns_dict[key],c="blue")
+        plt.savefig("plots/" + key + filename[:-4] + ".png")
+    returns_dict = {}
+    for key in l_five:
         returns_dict[key] = []
         for kline in l_three[key]:
             returns_dict[key].append(kline.following_candle.ratio)
